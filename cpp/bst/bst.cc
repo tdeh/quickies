2559f0f78d1e61ba1e944aea4aa0b38c02c28008
Taylor DeHaan <tdehaan93@gmail.com>
2017-08-16 19:56:01 -0600

    Implement BST remove method

--- cpp/bst/bst.cc
+++ cpp/bst/bst.cc
@@ -66,7 +66,86 @@ BstNode* Bst::Find(int val) {
 }
 
 void Bst::Remove(int val) {
-    // TODO
+    BstNode* node;
+    BstNode* parent = nullptr;
+
+    // Find node
+    while (node != nullptr) {
+        if (node->data == val) {
+            // Found the node
+            return node;
+        } else if (val <= node->data) {
+            // Traverse down left child
+            parent = node;
+            node = node->left;
+        } else {
+            // Traverse down right child
+            parent = node;
+            node = node->right;
+        }
+    }
+
+    // Check if node has children
+    if (!node->left && !node->right) {
+        // No children; trivial delete
+        if (!parent) {
+            // Node was root; set root to nullptr
+            root_ = nullptr;
+        } else if (node == parent->left) {
+            // Node was left child; update parent->left
+            parent->left = nullptr;
+        } else {
+            // Node was right child; update parent->right
+            parent->right = nullptr;
+        }
+        delete node;
+    } else if (!node->left) {
+        // Only has left children
+        if (!parent) {
+            // Node was root; set root to left children
+            root_ = node->left;
+        } else if (node == parent->left) {
+            // Node was left child; update parent->left
+            parent->left = node->left;
+        } else {
+            // Node was right child; update parent->right
+            parent->right = node->left;
+        }
+        delete node;
+    } else if (!node->right) {
+        // Only has right children
+        if (!parent) {
+            // Node was root; set root to right children
+            root_ = node->right;
+        } else if (node == parent->left) {
+            // Node was left child; update parent->left
+            parent->left = node->right;
+        } else {
+            // Node was right child; update parent->right
+            parent->right = node->right;
+        }
+        delete node;
+    } else {
+        // Has left and right children; complex delete
+
+        // Find inorder successor to node being deleted (smallest value in right subtree)
+        BstNode* successor = node->right;
+        BstNode* parent_successor = node;
+        while (successor->left) {
+            parent_successor = successor;
+            successor = successor->left;
+        }
+
+        // Handle successor's right subtree if it exists
+        if (successor->right) {
+            parent_successor->left = successor->right;
+        }
+
+        // Replace node we're deleting with the successor
+        successor->right = node->right;
+        successor->left = node->left;
+        delete node;
+    }
 }
 
 BstNode* Bst::GetRoot() {
